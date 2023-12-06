4ff784c78e5370b29597f1fcc3d3485bbd0a7937
Dominika Szymanska <34137726+nidomika@users.noreply.github.com>
2023-12-06 16:24:34 +0100

    Add functions

--- functions.py
+++ functions.py
@@ -14,3 +14,14 @@ def multiply(int_list):
     for digit in int_list:
         result *= digit
     return result
+
+
+# split a list into a list of n-element lists
+def split_list(l, n):  
+    for i in range(0, len(l), n):  
+        yield l[i:i + n]
+
+
+# join a list of ints and return int
+def join_list_int(int_list):
+    return int("".join(map(str, int_list)))
